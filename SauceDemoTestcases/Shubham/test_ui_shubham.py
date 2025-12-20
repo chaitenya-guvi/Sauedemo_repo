d423cf5b0b27b44134e047c27fbcb87bc2100581
Shubhambs89 <sb1249952@gmail.com>
2025-12-20 18:40:05 +0530

    Committed to Test Case 1

--- SauceDemoTestcases/Shubham/test_ui_shubham.py
+++ SauceDemoTestcases/Shubham/test_ui_shubham.py
@@ -1,41 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import pytest
 
-url="https://www.saucedemo.com/"
-#Open Google chrome
-driver = webdriver.Chrome()
+def test_ui_about():
+    url = "https://www.saucedemo.com/"
+    #Open Google chrome
+    driver = webdriver.Chrome()
 
-#Maximize the window
-driver.maximize_window()
+    #Maximize the window
+    driver.maximize_window()
 
-driver.get(url)
-sleep(2)
+    driver.get(url)
+    sleep(2)
 
-#Finding the Username and adding value
-webelement_of_email_input = driver.find_element(By.XPATH,"//input[@data-test='username']")
-webelement_of_email_input.send_keys("standard_user")
+    #Finding the Username and adding value
+    webelement_of_email_input = driver.find_element(By.XPATH, "//input[@data-test='username']")
+    webelement_of_email_input.send_keys("standard_user")
 
-#Finding the Password space and adding Value
-webelement_of_password_input = driver.find_element(By.XPATH,"//input[@data-test='password']")
-webelement_of_password_input.send_keys("secret_sauce")
+    #Finding the Password space and adding Value
+    webelement_of_password_input = driver.find_element(By.XPATH, "//input[@data-test='password']")
+    webelement_of_password_input.send_keys("secret_sauce")
 
-# clicking on the Login Button
-login_btn = driver.find_element(By.ID, "login-button")
-login_btn.click()
-sleep(5)
+    # clicking on the Login Button
+    login_btn = driver.find_element(By.ID, "login-button")
+    login_btn.click()
+    sleep(5)
 
-#Clicking on the menu button
-webelement_of_menu = driver.find_element(By.ID, "react-burger-menu-btn")
-webelement_of_menu.click()
-sleep(2)
+    #Clicking on the menu button
+    webelement_of_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+    webelement_of_menu.click()
+    sleep(2)
 
-#Click o the About link
-webelement_of_about = driver.find_element(By.ID, "about_sidebar_link")
-webelement_of_about.click()
+    #Click o the About link
+    webelement_of_about = driver.find_element(By.ID, "about_sidebar_link")
+    webelement_of_about.click()
 
-assert driver.current_url == "https://saucelabs.com/"
-print("Test Case 1 Passed")
-#close browser
-sleep(5)
-driver.quit()
+    assert driver.current_url == "https://saucelabs.com/"
+    print("Test Case 1 Passed")
+    #close browser
+    sleep(5)
+    driver.quit()
